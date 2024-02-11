abd1b172d5f25fd8d78daf8858133d5f7949b508
nepulkumar <124512972+nepulkumar@users.noreply.github.com>
2024-02-11 21:19:15 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,55 @@
+
+// --hours
+// 12 hour=360deg
+// 1hour=360/12=30
+// h hours=30h+
+
+
+// min--
+// 60min=30
+// 1m=30/60=1/2
+// m mins=(1/2)m
+
+
+// 60min=360deg
+// 1min=360/60=6deg
+// m min=6m
+
+// sec--
+// 60sec=369deg
+// 1sec=360/60=6deg
+// s secs=6s
+
+
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+
+    //gattimg hour,mins secs from to take
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    // hr.style.transform = 'rotate({hRotation}deg)';
+    // min.style.transform = 'rotate({mRotation}deg)';
+    // sec.style.transform = 'rotate({sRotation}deg)';
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+
+
+
+
+}
+
+setInterval(displayTime, 1000);
